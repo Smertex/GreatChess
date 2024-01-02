75e6c53591632c1e4d84e83cc2a127ca96b538e6
Smertex <kiratsvetkova102@gmail.com>
2024-01-02 07:12:03 +0300

    Изменил цвета фигур

--- src/main/java/org/example/Window/RenderingBoard.java
+++ src/main/java/org/example/Window/RenderingBoard.java
@@ -14,10 +14,11 @@ public class RenderingBoard {
     public GridPane boardRendering(Board board){
         GridPane boardGUI = new GridPane();
 
+        int sizeSquare = 100;
+
         for(int i = 0; i <= 9; i++){
             for(int j = 0; j <= 9; j++){
-                Rectangle rectangle = new Rectangle(60, 60);
-
+                Rectangle rectangle = new Rectangle(sizeSquare, sizeSquare);
 
                 if(!Board.isSquareDark(new Coordinate(i, j))){
                     rectangle.setFill(Color.GREY);
@@ -29,7 +30,16 @@ public class RenderingBoard {
 
                 if(board.getFigure(i, j) != null){
                     Text image = new Text(String.valueOf(board.getFigure(i, j).getImage()));
-                    image.setFont(Font.font(rectangle.getHeight() / 2));
+                    image.setFont(Font.font((double) sizeSquare / 2));
+
+
+                    if(board.getFigure(i, j).getColor()){
+                        image.setFill(Color.WHITE);
+                        image.setStrokeWidth(0.5);
+                        image.setStroke(Color.BLACK);
+                    } else {
+                        image.setFill(Color.BLACK);
+                    }
 
                     boardGUI.add(image, j, i);
                     GridPane.setHalignment(image, HPos.CENTER);
